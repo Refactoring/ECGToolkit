a5bdafeac8d045c50debd38986a87d1b45f7397c
mvanettinger <mvanettinger@users.sourceforge.net>
2010-04-06 13:29:19 +0000

    *** empty log message ***

--- libs/ECGConversion/ECGConversion/UnknownECGReader.cs
+++ libs/ECGConversion/ECGConversion/UnknownECGReader.cs
@@ -1,5 +1,5 @@
 /***************************************************************************
-Copyright (c) 2004-2009, Thoraxcentrum, Erasmus MC, Rotterdam, The Netherlands
+Copyright (c) 2004-2010, Thoraxcentrum, Erasmus MC, Rotterdam, The Netherlands
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
--- libs/ECGConversion/PDF/PDF/PDFDemographics.cs
+++ libs/ECGConversion/PDF/PDF/PDFDemographics.cs
@@ -1,5 +1,5 @@
 /***************************************************************************
-Copyright 2008, Thoraxcentrum, Erasmus MC, Rotterdam, The Netherlands
+Copyright 2008,2010, Thoraxcentrum, Erasmus MC, Rotterdam, The Netherlands
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
