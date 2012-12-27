597b5686b089875c2f390801e6354869b77f30e7
mvanettinger <mvanettinger@users.sourceforge.net>
2012-12-27 17:31:05 +0000

    Small error in the version info.

--- libs/ECGConversion/ISHNE/AssemblyInfo.cs
+++ libs/ECGConversion/ISHNE/AssemblyInfo.cs
@@ -43,7 +43,7 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("2.2.*")]
+[assembly: AssemblyVersion("2.3.*")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
